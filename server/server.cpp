6a98712ccafa27d56a21840c5bb813af1cf4c876
ligang-chn <ligang.she@gmail.com>
2020-06-25 17:57:12 +0800

    select function

--- server/server.cpp
+++ server/server.cpp
@@ -9,8 +9,53 @@
 #include <iostream>
 #include <inaddr.h>
 
+#include <vector>
+#include <algorithm>
+
 #include "../DataStruct.h"
 
+std::vector<SOCKET> g_clients;
+
+int processor(SOCKET _cSock){
+    char szRecv[1024]={};//接收缓冲区
+    int nLen=recv(_cSock,(char*)&szRecv, sizeof(DataHeader),0);//数据先接收包头大小
+    DataHeader* header=(DataHeader*)szRecv;
+    if(nLen<=0){
+        std::cout<<"客户端退出~~~~~"<<std::endl;
+        return -1;
+    }
+
+    ///6）处理请求
+    switch (header->cmd){
+        case CMD_LOGIN:
+        {
+            recv(_cSock,szRecv+ sizeof(DataHeader), header->dataLength- sizeof(DataHeader),0);
+            Login* login=(Login*)szRecv;
+            std::cout<<"收到命令：CMD_LOGIN ,数据长度："<<login->dataLength
+                     <<" ,username: "<<login->userName<<" ,password: "<<login->PassWord<<std::endl;
+            //忽略判断用户密码是否正确的过程
+            LoginResult ret;
+            send(_cSock,(char*)&ret, sizeof(LoginResult),0);
+        }
+            break;
+        case CMD_LOGOUT:
+        {
+            recv(_cSock,szRecv+ sizeof(DataHeader), header->dataLength- sizeof(DataHeader),0);
+            Logout* logout=(Logout*)szRecv;
+            std::cout<<"收到命令：CMD_LOGIN ,数据长度："<<logout->dataLength
+                     <<" ,username: "<<logout->userName<<std::endl;
+            //忽略判断用户密码是否正确的过程
+            LogoutResult ret;
+            send(_cSock,(char*)&ret, sizeof(LogoutResult),0);
+        }
+            break;
+        default:
+            DataHeader header={CMD_ERROR,0};
+            send(_cSock,(char*)&header, sizeof(DataHeader),0);
+            break;
+    }
+    return 0;
+}
 
 int main() {
     WORD ver=MAKEWORD(2,2);//版本号
@@ -35,57 +80,58 @@ int main() {
         std::cout<<"Start Listening~~~~"<<std::endl;
     }
 
-    ///4)accept 等待接收客户端连接
-    sockaddr_in clientAddr={};//存放返回的客户端地址
-    int nAddrLen= sizeof(sockaddr_in);
-    SOCKET _cSock=INVALID_SOCKET;
-
-//    char msgBuf[]="I'm Server.";
-    _cSock=accept(_sock,(sockaddr*)&clientAddr, &nAddrLen);
-    if(_cSock==INVALID_SOCKET){
-        std::cout<<"ERROR-->INVALID_SOCKET!"<<std::endl;
-    }
-    std::cout<<"新客户端加入：IP = "<<inet_ntoa(clientAddr.sin_addr)<<" ,Port= "<<clientAddr.sin_port<<std::endl;
 
     while (true){
-        ///5）接收客户端数据
-        char szRecv[1024]={};//接收缓冲区
-        int nLen=recv(_cSock,(char*)&szRecv, sizeof(DataHeader),0);//数据先接收包头大小
-        DataHeader* header=(DataHeader*)szRecv;
-        if(nLen<=0){
-            std::cout<<"客户端退出~~~~~"<<std::endl;
+        fd_set fdRead;
+        fd_set fdWrite;
+        fd_set fdExp;
+
+        FD_ZERO(&fdRead);
+        FD_ZERO(&fdWrite);
+        FD_ZERO(&fdExp);
+
+        FD_SET(_sock,&fdRead);
+        FD_SET(_sock,&fdWrite);
+        FD_SET(_sock,&fdExp);
+
+        for(int n=(int)g_clients.size()-1;n>=0;n--){
+            FD_SET(g_clients[n],&fdRead);
+        }
+        ///nfds是一个整数值，是指fd_set集合中所有描述符（socket）的范围，而不是数量
+        ///即所有文件描述符最大值+1，在windows中这个参数无所谓，可以写0
+        int ret=select(_sock+1,&fdRead,&fdWrite,&fdExp, nullptr);
+        if(ret<0){
+            std::cout<<"select任务结束~~~~~"<<std::endl;
             break;
         }
+        if(FD_ISSET(_sock,&fdRead)){
+            FD_CLR(_sock,&fdRead);
+            ///4)accept 等待接收客户端连接
+            sockaddr_in clientAddr={};//存放返回的客户端地址
+            int nAddrLen= sizeof(sockaddr_in);
+            SOCKET _cSock=INVALID_SOCKET;
+            _cSock=accept(_sock,(sockaddr*)&clientAddr, &nAddrLen);
+            if(_cSock==INVALID_SOCKET){
+                std::cout<<"ERROR-->INVALID_SOCKET!"<<std::endl;
+            }
+            g_clients.push_back(_cSock);
+            std::cout<<"新客户端加入：IP = "<<inet_ntoa(clientAddr.sin_addr)<<" ,Port= "<<(int)_cSock<<std::endl;
 
-        ///6）处理请求
-        switch (header->cmd){
-            case CMD_LOGIN:
-                {
-                    recv(_cSock,szRecv+ sizeof(DataHeader), header->dataLength- sizeof(DataHeader),0);
-                    Login* login=(Login*)szRecv;
-                    std::cout<<"收到命令：CMD_LOGIN ,数据长度："<<login->dataLength
-                                <<" ,username: "<<login->userName<<" ,password: "<<login->PassWord<<std::endl;
-                    //忽略判断用户密码是否正确的过程
-                    LoginResult ret;
-                    send(_cSock,(char*)&ret, sizeof(LoginResult),0);
-                }
-                break;
-            case CMD_LOGOUT:
-                {
-                    recv(_cSock,szRecv+ sizeof(DataHeader), header->dataLength- sizeof(DataHeader),0);
-                    Logout* logout=(Logout*)szRecv;
-                    std::cout<<"收到命令：CMD_LOGIN ,数据长度："<<logout->dataLength
-                             <<" ,username: "<<logout->userName<<std::endl;
-                    //忽略判断用户密码是否正确的过程
-                    LogoutResult ret;
-                    send(_cSock,(char*)&ret, sizeof(LogoutResult),0);
-                }
-                break;
-            default:
-                DataHeader header={CMD_ERROR,0};
-                send(_cSock,(char*)&header, sizeof(DataHeader),0);
-                break;
         }
+
+        ///5）接收客户端数据
+        for(int n=0;n<fdRead.fd_count;n++){
+            if(-1==processor(fdRead.fd_array[n])){
+                auto iter=std::find(g_clients.begin(),g_clients.end(),fdRead.fd_array[n]);
+                if(iter!=g_clients.end()){
+                    g_clients.erase(iter);
+                }
+            }
+        }
+    }
+
+    for(size_t n=g_clients.size()-1;n>=0;n--){
+        closesocket(g_clients[n]);
     }
     ///8)关闭套接字
     closesocket(_sock);
